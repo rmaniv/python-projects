58eda7cd5bdd55eb9ed9f749b4eff27883bbf702
Vinamr Loomba Sachdeva <34504513+vinamrsachdeva@users.noreply.github.com>
2020-11-01 21:48:18 +0530

    Create primefinder.py

---
+++ primefinder.py
@@ -0,0 +1,19 @@
+
+n = int(input())
+p = [2]
+d = 0
+
+for i in range(3,n+1):
+    d = 0
+    for j in range(2,i):
+        if i%j == 0:
+            d+=1
+    if d == 0:
+        p.append(i)
+
+print("Prime Numbers: ")
+
+for x in p:
+    print(x)
+
+print("There are ", len(p), " prime numbers till ", n)
